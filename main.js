4652c5562f803d18efe7c984ba18a50a618ec467
Timothy Harley <timothy.d.harley@gmail.com>
2018-09-19 22:50:55 -0500

    student form wip

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log ('main.js is connected')
+const startButton = document.getElementById('startButton');
+const jumbotron = document.getElementById('jumbotron');
+const hatForm = document.getElementById('hatForm')
+
+startButton.addEventListener("click", (e) => {
+    jumbotron.remove();
+    hatForm.style.display = 'block';
+  })
